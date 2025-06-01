96773411f695403fc7e5aabb8d2e1221d181f2bc
wilmahammarshall <wilmahammarshall@gmail.com>
2025-06-01 22:01:26 +0200

    css

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,13 +20,17 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2018/06/21',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
-    });
+     jQuery("#clock").countdown("2025/07/25 21:00:00", function (event) {
+       var $this = jQuery(this).html(
+         event.strftime(
+           "" +
+             '<div class="time-entry days"><span>%-D</span> Days</div> ' +
+             '<div class="time-entry hours"><span>%H</span> Hours</div> ' +
+             '<div class="time-entry minutes"><span>%M</span> Minutes</div> ' +
+             '<div class="time-entry seconds"><span>%S</span> Seconds</div> '
+         )
+       );
+     });
 
     /* slicknav mobile menu active  */
     $('.mobile-menu').slicknav({
